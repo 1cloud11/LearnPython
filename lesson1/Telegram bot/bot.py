0ee7160938003aa41e3da5b8aa462aa20fcfe6a1
Kot Vladislav <kot.vladislav@ukr.net>
2020-11-23 22:37:25 +0200

    Bugfix with wrong logging encoding

--- lesson1/Telegram bot/bot.py
+++ lesson1/Telegram bot/bot.py
@@ -3,7 +3,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+#logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(stream=open(r'D:\Coding\LearnPython\lesson1\Telegram bot\bot.log', 'w', encoding='utf-8'), level=logging.INFO)
 
 #PROXY = {'proxy_url': settings.PROXY_URL,
 #       'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME , 'password': settings.PROXY_PASSWORD }}
